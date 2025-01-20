1f99eb8ecd25d6d6ed88821cabca77681a570baf
xyfuil <xyfuil@qq.com>
2025-01-20 21:51:15 +0800

    update

--- api/controllers/IpController.js
+++ api/controllers/IpController.js
@@ -2,6 +2,7 @@ var IpController = {};
 
 IpController['getResult'] = function (req, res) {
   var ip = req.ip;
+  console.log(req.headers['x-forwarded-for']);
   return res.render('ip', {
     ip: ip
   });
